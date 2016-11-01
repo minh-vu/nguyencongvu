dc5bd530de119558eef2cf2d523eb94aacf8ce66
Vu Nguyen <Vu Nguyen>
2016-11-02 01:53:07 +0700

    update

---
+++ assets/setting.js
@@ -0,0 +1,72 @@
+// ============= HOME ==============
+var Page = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "",
+    subHeading: "Website để thể hiện. <strong>Landing page</strong> để thu hút khách hàng",
+    mainHeading: "<b>Email us now.</b> Own a <b>Landing page</b> in 24hrs",
+    CTA: "Tôi muốn có ngay Landing Page",
+    brief: "Cơ hội nhận discount or Free Landing Page. Chỉ cần theo dõi các chương trình cộng đồng của chúng tôi",
+
+    // ============= VARS ==============
+    url : "http://www.thietkewebnhanh.top/", //-- co slash cuoi /
+    mainkeyWord : "mở kênh online marketing",
+    longkeyWords : ["mở kênh online nhanh chóng", "thu hút khách hàng online"],
+    facebookAppId : "303370230021409", //-- your appID
+    googleAnalyticId : "UA-82197343-2", //-- your id
+    formId: "https://docs.google.com/forms/d/e/1FAIpQLScjBXL-M0eRB0qhX1xSV5sWWAwsXbNqtjAGm3aq0-l5EZAisg/viewform?embedded=true",
+}
+
+var Social = {
+    //-- de trong "" de giu nguyen default text
+    email: "maito:",
+    facebook: "1",
+    twitter: "2",
+    google: "3",
+    instagram: "3",
+    pinterest: "0",
+    linkedin: "0"
+}
+
+var colors = [
+    "red","blue","green","orange",  // 0 1 2 3  
+    "pink", "purple", "light-blue", "cyan", // 4 5 6 7  
+    "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
+    "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
+    "brown", "grey", "blue-grey"      // 16 17 18 19
+];
+
+// colors = ['green', 'deep-purple']
+var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
+var chooseColor = colors[i].toString();
+
+
+// ============= PERSONAL ==============
+var Person = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Nguyễn Công Vũ",
+    fullname: "<strong>Nguyễn Công Vũ</strong>.",
+    slogan: "Unique. Sports. Professional.",
+    sex: '<i class="fa fa-male"></i>', //-- male, female
+    phone: 'tel:0937980003',
+    // message: "Tôi đã sẵn sàng, còn bạn thì sao?", //-- thong diep chinh hien nay!
+    message: "",
+    
+    facebook: 'https://facebook.com/nguyencongvu',
+    twitter: "https://twitter.com/vunc",
+    google: "https://plus.google.com/nguyencongvu",
+    instagram: "https://instagram.com/nguyencongvu",
+    pinterest: "https://pinterest.com/nguyencongvu",
+    linkedin: "https://linkedin.com/nguyencongvu",
+    google: "https://plus.google.com/nguyencongvu",
+    CTA: "Call me",
+
+}
+
+
+var Social = {
+
+}
+
+
+
+    
